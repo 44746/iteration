9b4e19f6207827e9999a1fc5e41bc2a5ccb986b5
georgewest <georgewest@georges-mbp>
2014-10-13 21:28:15 +0100

    iteration r&r

---
+++ secret password.py
@@ -0,0 +1,10 @@
+#George West
+#secret password
+
+password = input("Please enter the password: ")
+while password != 'secret':
+    print("Try again")
+    password = input("Please enter the password: ")
+print('You now have access to the system')
+
+
